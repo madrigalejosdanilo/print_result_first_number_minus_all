05d3abc8f579b72bb2aa2b4db46c33223ec42ef8
Danilo Madrigalejos <danilo.madrigalejos@vipre.com>
2025-03-08 11:50:18 +0800

    ask user for 10 numbers

--- print_result_first_number_minus_all.py
+++ print_result_first_number_minus_all.py
@@ -1,4 +1,16 @@
 # ask user for 10 numbers
+
+num1 = int(input("Please input a number: "))
+num2 = int(input("Please input a number: "))
+num3 = int(input("Please input a number: "))
+num4 = int(input("Please input a number: "))
+num5 = int(input("Please input a number: "))
+num6 = int(input("Please input a number: "))
+num7 = int(input("Please input a number: "))
+num8 = int(input("Please input a number: "))
+num9 = int(input("Please input a number: "))
+num10 = int(input("Please input a number: "))
+
 # first number - 2nd
 # result - 3rd
 # result - 4th
@@ -8,4 +20,3 @@
 # result - 8th
 # result - 9th
 # result - 10th
-
